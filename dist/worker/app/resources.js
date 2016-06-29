0425ecc41774fc57b4b856ed43687e6c3d1ffdc7
Simon Bobrov <simon.bobrov@windeln.de>
2016-06-29 14:57:51 +0200

    added baseUrl support

---
+++ dist/worker/app/resources.js
@@ -0,0 +1,4 @@
+"use strict";
+var page_resource_1 = require('./resources/page.resource');
+exports.PageResource = page_resource_1.PageResource;
+//# sourceMappingURL=resources.js.map
---
+++ dist/worker/app/services.js
@@ -0,0 +1,6 @@
+"use strict";
+var controls_service_1 = require('./services/controls.service');
+exports.ControlsService = controls_service_1.ControlsService;
+var pages_service_1 = require('./services/pages.service');
+exports.PagesService = pages_service_1.PagesService;
+//# sourceMappingURL=services.js.map
